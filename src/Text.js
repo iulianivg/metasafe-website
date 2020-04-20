3212d2a46a832de240fa77de2268089928e6baae
0xCristianOff <ivighenie@yahoo.com>
2020-04-20 23:06:33 +0100

    minor adjustment to word on using metasafe tool

--- src/Text.js
+++ src/Text.js
@@ -343,7 +343,7 @@ generateMnemonic24 = async() => {
       return (
           <div>
                 <h2>Mnemonic Analyser</h2>
-                <p style={{color:'grey'}}>This tool is client side and will not send your mnemonic data to our AI or servers</p>
+                <p style={{color:'grey'}}>For security reasons you should use our offline <a href="/recover">tool</a></p>
         <Grid container spacing={3}>
         {/* <Grid item xs={12} sm={3}>
         <p>Analyse how secure your mnemonic is. </p>
